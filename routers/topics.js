b42a5e4d13ae395ed7d35884bec70c7445a5768c
grakify90 <ninavanes1990@hotmail.com>
2020-08-11 11:17:35 +0200

    made some endpoints for trips, topics, and updatting user

--- routers/topics.js
+++ routers/topics.js
@@ -5,10 +5,11 @@ const User = require("../models").user;
 
 const router = new Router();
 
+//fetch all topics, including the user who created the topic, and all the replies to that topic
 router.get("/", async (req, res, next) => {
   try {
     const allTopics = await Topic.findAll({
-      include: [{ model: Reply }],
+      include: [{ model: Reply }, { model: User }],
     });
     res.send(allTopics);
   } catch (error) {
@@ -16,11 +17,12 @@ router.get("/", async (req, res, next) => {
   }
 });
 
+//fetch one topic, including user who created the topic, and all the replies to that topic, including the users who created the replies
 router.get("/:topicId", async (req, res, next) => {
   try {
     const topicId = req.params.topicId;
     const specificTopic = await Topic.findByPk(topicId, {
-      include: [{ model: Reply }, { model: User }],
+      include: [{ model: Reply, include: [User] }, { model: User }],
     });
     if (specificTopic) {
       res.send(specificTopic);
