ea8a17b917e5e08ab1f0c10dad1a58df5ce7d079
grakify90 <ninavanes1990@hotmail.com>
2020-08-11 15:10:19 +0200

    setup basic endpoints complete, will add more later. Made overview of all andpoints in README

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# Portfolio_project_backend
+## Cycle Mate Backend
+
+# All Endpoints
+
+**USER**
+
+- POST /login = get token
+- POST /signup = creates new user and get token
+- 🔒 GET /me = get own data
+- 🔒 PATCH /update = update own user data
+- 🔒 PATCH /updatepassword = update own password
+
+**TOPICS**
+
+- GET /topics get = all topics (user (topic creator) included, replies included)
+- GET /topics/:topicId = one topic (user (topic creator) included, replies included and user (reply creators) included)
+- 🔒 POST /topics = create a topic for logged in user
+- 🔒 PATCH /topics/:topicId = update a topic for logged in user
+
+**REPLIES**
+
+- 🔒 POST /replies/:topicId = create a reply to topic for logged in user
+- 🔒 PATCH /replies/:replyId = update a reply for logged in user
+- 🔒 DELETE /replies/:replyId = delete a reply for logged in user
+
+**TRIPS**
+
+- GET /trips = all trips including user (owner) and user (participants)
+- GET /trips/:tripId = one trip including user (owner) and user (participants)
+- 🔒 POST /trips = create a trip for logged in user and add user as first participant to this trip
+
+**PARTICIPANTS**
+
+- 🔒 POST /participants/:tripId = create new participant (logged in user) for specific trip
+- 🔒 DELETE /participants/:tripId = delete participant (logged in user) for specific trip
--- routers/topics.js
+++ routers/topics.js
@@ -2,6 +2,7 @@ const { Router } = require("express");
 const Topic = require("../models").topic;
 const Reply = require("../models").reply;
 const User = require("../models").user;
+const authMiddleware = require("../auth/middleware");
 
 const router = new Router();
 
@@ -13,7 +14,7 @@ router.get("/", async (req, res, next) => {
     });
     res.send(allTopics);
   } catch (error) {
-    console.log(error.message);
+    return res.status(400).send(error.message);
   }
 });
 
@@ -30,7 +31,52 @@ router.get("/:topicId", async (req, res, next) => {
       res.status(404).send({ message: "Topic not found" });
     }
   } catch (error) {
-    console.log(error.message);
+    return res.status(400).send(error.message);
+  }
+});
+
+//post a new topic for logged in user
+router.post("/", authMiddleware, async (req, res, next) => {
+  try {
+    const { title, content, imageUrl } = req.body;
+    if (!title || !content) {
+      res
+        .status(400)
+        .send({ message: "Please provide a title and content for topic" });
+    } else {
+      const userOwningTopic = req.user.id;
+      const newTopic = await Topic.create({
+        userId: userOwningTopic,
+        title,
+        content,
+        imageUrl,
+      });
+      res.send(newTopic);
+    }
+  } catch (error) {
+    return res.status(400).send(error.message);
+  }
+});
+
+//updating a topic for logged in user
+router.patch("/:topicId", authMiddleware, async (req, res, next) => {
+  try {
+    const topicToBeUpdatedId = req.params.topicId;
+    const userOwningTopic = req.user.id;
+    const topicToBeUpdated = await Topic.findByPk(topicToBeUpdatedId);
+    if (userOwningTopic === topicToBeUpdated.userId) {
+      const updatedTopic = await topicToBeUpdated.update({
+        ...req.body,
+        userId: userOwningTopic,
+      });
+      res.send(updatedTopic);
+    } else {
+      res
+        .status(401)
+        .send({ message: "Only topicstarter can make changes to the topic" });
+    }
+  } catch (error) {
+    return res.status(400).send(error.message);
   }
 });
 
