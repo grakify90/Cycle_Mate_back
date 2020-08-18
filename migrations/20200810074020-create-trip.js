6595e2bbec6a72cc22d960d23a099d5d859d940d
grakify90 <ninavanes1990@hotmail.com>
2020-08-18 11:46:47 +0200

    added column precise to trip to identify successful geocoding

--- migrations/20200810074020-create-trip.js
+++ migrations/20200810074020-create-trip.js
@@ -28,6 +28,10 @@ module.exports = {
         type: Sequelize.STRING,
         allowNull: false,
       },
+      precise: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+      },
       latitude: {
         type: Sequelize.FLOAT,
         allowNull: false,
