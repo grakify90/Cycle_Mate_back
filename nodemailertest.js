59ccb5966aa5352d0739768b457604586f790bfd
grakify90 <ninavanes1990@hotmail.com>
2020-08-20 11:30:27 +0200

    added comments and kept nodemailer test file but removed certain info

--- nodemailertest.js
+++ nodemailertest.js
@@ -1,36 +1,37 @@
 const nodemailer = require("nodemailer");
 const hbs = require("nodemailer-express-handlebars");
-require("dotenv").config();
+// require("dotenv").config();
 
 let transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: process.env.EMAIL_GMAIL,
-    pass: process.env.PASSWORD_GMAIL,
+    user: { email },
+    pass: { password },
   },
   debug: true, // show debug output
   logger: true, // log information in console
 });
 
+//using template
 transporter.use(
   "compile",
   hbs({
     viewEngine: "express-handlebars",
-    //CHANGE TO CORRECT PATH!
+    //change to correct path
     viewPath: "./",
   })
 );
 
 let mailOptions = {
   from: '"Cycle Mate" <cyclemate137@gmail.com>',
-  to: "ninavanes1990@hotmail.com",
+  to: "pass email here",
   subject: "Welcome to Cycle Mate!",
   text: "Plain text version of the html",
   attachments: [{ filename: "cyclemate.jpg", path: "./cyclemate.jpg" }],
   template: "main",
   //Passing variables to email template
   context: {
-    name: "Nina",
+    name: "Test test",
   },
 };
 
